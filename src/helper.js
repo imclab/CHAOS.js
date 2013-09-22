cbd2e2761f9aa5ca9e961c91947d69f304bc6b95
Okanovic Dragan <dragan.okan@gmail.com>
2013-09-22 17:02:50 +0200

    obj parser, improved texture loading

--- src/helper.js
+++ src/helper.js
@@ -11,10 +11,10 @@ var CHAOS = CHAOS || {};
 CHAOS.log = function(){try{console.log.apply(console,arguments)}catch(e){try{opera.postError.apply(opera,arguments)}catch(e){alert(Array.prototype.join.call(arguments," "))}}};
 CHAOS.getInfo=function(){var e="",t="";var n=0,r=0;for(var i in this){if(i!="getInfo"){if(typeof this[i]=="function"){r++;var s=new RegExp("function[^(]*(([^)]*))","i");var o=this[i].toString();var u=o.match(s)[0];u=u.replace(/function[^(]*\(/,"");t+=i+"("+"), "}else{n++;e+=i+", "}}}return"CHAOS :<br>ATTRIBUTES {"+n+"}: "+e+"<br><br>"+"METHODS {"+r+"}: "+t};
 CHAOS.getClasses=function(){var s="";for(var t in this){if(t!="log"&&t!="getInfo"&&t!="getClasses")s+=s?", "+t:t}return s};
-CHAOS.htmltojs=function(s){return s.replace(/<\/?br>/gi, "\n");};
+CHAOS.br2nl=function(s){s=s.replace(/<\/?br>/gi, "\n");return s.replace(/&[^;]+;/g, " ");};
 
 // CHAOS.Color
-// CHAOS.Texture   	::: video texture, modifiers (clamping, size...)
+// CHAOS.Texture 		::: video texture, modifiers (clamping, size...)
 // CHAOS.loadFile() ??
 
 CHAOS.Color = function(hex) {
@@ -102,6 +102,7 @@ CHAOS.Texture = function() {
 	this.image = null;
 	this.texture = null;
 	this.faces = [];
+	this.callback = null;
 	this.name = "texture_"+arguments.callee._count++;
 	return this;
 };
@@ -112,7 +113,7 @@ CHAOS.Texture.prototype = {
 
 			this.texture = gl.createTexture();
 			gl.bindTexture(gl.TEXTURE_2D, this.texture);
-			// gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
@@ -125,7 +126,10 @@ CHAOS.Texture.prototype = {
 		this.gl = gl;
 		return this;
 	},
-	load2D: function(url) {
+	load2D: function(params) {
+
+		var url = params.image ? params.image : params.url;
+		this.callback = params.callback ? params.callback : null;
 		
 		this.isReady = false;
 		this.isPrepared = false;
@@ -139,6 +143,7 @@ CHAOS.Texture.prototype = {
 		this.image.onload = function() {
 			self.isReady = true;
 			self.isPrepared = false;
+			if(self.callback) { self.callback(); }
 		};
 
 		return this;
@@ -174,7 +179,9 @@ CHAOS.Texture.prototype = {
 
 		    gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
 
-		    if(pp==6) { this.isPrepared = true; }
+		    if(pp==6) {
+		    	this.isPrepared = true;
+		    }
 		}
 
 		this.gl = gl;
@@ -187,6 +194,7 @@ CHAOS.Texture.prototype = {
 		this.isPrepared = false;
 		this.faces = [];
 		this.type = "cube";
+		this.callback = list.callback ? list.callback : null;
 
 		var ready = 0;
 		var url_all = url_all || "";	
@@ -207,6 +215,7 @@ CHAOS.Texture.prototype = {
 	                ready++;
 	                if(ready==6) {
 	                	self.isReady = true;
+	                	if(self.callback) { self.callback(); }
 	                }
 	        };
 	        this.faces[i].src = srcs[i];      
@@ -216,3 +225,428 @@ CHAOS.Texture.prototype = {
 };
 
 CHAOS.Texture._count = 0;
+
+
+/*CHAOS.loadOBJ = function(params) {
+
+		var file = params.file ? params.file : params.obj;
+		var matlib = params.matlib ? params.matlib : file.replace(/\.obj/, ".mtl");
+
+		var obj_text = "",
+			mat_text = "";
+
+		var obj_meshes = [],
+			obj_geometries = [],
+			obj_materials = [];
+
+		var current_mesh = 0,
+			current_material = 0;
+
+		var vertices = [],
+			uvs = [],
+			normals = [];
+
+		// comments
+		var reg_comm = /(#[^\n]*\n)/g;
+
+		// grouping
+		var reg_o = /#___o\s([^\n]+)([^#]+)/g,
+			reg_g = /#___g\s([^\n]+)([^#]+)/g;
+
+		// attributes
+		var reg_v  = /v[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g,
+			reg_vt = /vt[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g,
+			reg_vn = /vn[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g;
+
+		function parse_mat(mat_str, callback, arg) {
+			mat_str.replace(/newmtl\s+(.+)/g, function(match, mat_name) {
+				obj_materials[mat_name] = new CHAOS.Material();
+			});
+
+			// for(var key in obj_materials) {
+			// 	CHAOS.log(key, obj_materials[key]);
+			// }
+
+			return callback(arg);
+		}
+
+		function parse_obj(otext) {
+
+			function extractVertex(match, x, y, z) {
+				vertices.push(new CHAOS.Vertex( parseFloat(x), parseFloat(y), parseFloat(z) ));
+				return "";
+			}
+
+			function extractUV(match, s, t) {
+				uvs.push(new CHAOS.UV( parseFloat(s), parseFloat(t) ));
+				return "";
+			}
+
+			function extractNormal(match, dx, dy, dz) {
+				normals.push(new CHAOS.Vec3( parseFloat(dx), parseFloat(dy), parseFloat(dz) ));
+				return "";
+			}
+
+			function extractObject(match, name, object_data) {
+
+				obj_meshes[name] = new CHAOS.Mesh(new CHAOS.Geometry(), null);
+
+				current_mesh = name;
+				CHAOS.log(obj_meshes[current_mesh]);
+
+				var geom_vertices = [],
+					geom_uvs = [],
+					geom_normals = [],
+					geom_indices = [],
+					part_v_indices = [],
+					part_t_indices = [],
+					part_n_indices = [];
+
+				var reg_f = /f\s+(.+)/g,
+					reg_slash = /\/{1}/g,
+					reg_f_vvv = /(\-?\d+)\/(\-?\d+)\/(\-?\d+)/,
+					reg_f_vt  = /(\-?\d+)\/(\-?\d+)\s+(\-?\d+)\/(\-?\d+)\s+(\-?\d+)\/(\-?\d+)/,
+					reg_f_vtn = /(\-?\d+)\/(.*)\/(\-?\d+)\s+(\-?\d+)\/(.*)\/(\-?\d+)\s+(\-?\d+)\/(.*)\/(\-?\d+)/;
+
+				object_data = object_data.replace(/\bg\b/g, "#___g");
+				if(object_data.match("#___g")==null) {
+					object_data = "#___g group_1\n"+object_data;
+				}
+
+				object_data = object_data.replace(reg_g, function(match, group_name, faces_data) {
+
+					var reg_mat = /usemtl[\t ]+(.+)/;
+
+					// nadji grupu
+					// materijal - novi ili isti
+					// grupu ->
+					//    dodaj atribute i faces
+
+					var mats = [];
+					faces_data = faces_data.replace(reg_mat, function(match, name) { mats.push(name); } )
+					var mat_name = null;
+					if(mats.length!=0) {
+						mat_name = mats[0];
+						CHAOS.log(group_name +" uses " +mat_name+ " material.");
+					}
+					else {
+						mat_name = current_material;
+					}
+					current_material = mat_name;
+
+					faces_data.replace(reg_f, function(match, str) {
+
+						var num_slashes = str.match(reg_slash).length;
+						switch(num_slashes) {
+							case 2:
+								str.replace(reg_f_vvv, function(match, v1, v2, n3) {
+									v1 = parseInt(v1); v1 = (v1<0) ? vertices.length-v1 : v1-1;
+									v2 = parseInt(v2); v2 = (v2<0) ? vertices.length-v2 : v2-1;
+									n3 = parseInt(n3); n3 = (n3<0) ? vertices.length-n3 : n3-1;
+
+									if(part_v_indices.indexOf(v1)==-1) { part_v_indices.push(v1); geom_vertices.push(vertices[part_v_indices.indexOf(v1)]); }
+									if(part_v_indices.indexOf(v2)==-1) { part_v_indices.push(v2); geom_vertices.push(vertices[part_v_indices.indexOf(v2)]); }
+									if(part_v_indices.indexOf(n3)==-1) { part_v_indices.push(n3); geom_vertices.push(vertices[part_v_indices.indexOf(n3)]); }
+
+									geom_indices.push([v1, v2, n3]);
+
+									return "";
+								});
+								break;
+							case 3:
+								str.replace(reg_f_vt, function(match, v1, t1, v2, t2, n3, t3) {
+									//
+									// TODO
+									//
+
+									return "";
+								});
+								break;
+							case 6:
+							
+								str.replace(reg_f_vtn, function(match, v1, t1, n1, v2, t2, n2, v3, t3, n3) {
+									// CHAOS.log("SHIT", v1,v2,v3, t1,t2,t3, n1,n2,n3);
+									v1 = parseFloat(v1); v1 = (v1<0) ? vertices.length-v1 : v1-1;
+									v2 = parseFloat(v2); v2 = (v2<0) ? vertices.length-v2 : v2-1;
+									v3 = parseFloat(v3); v3 = (v3<0) ? vertices.length-v3 : v3-1;
+
+									
+
+									n1 = parseFloat(n1); n1 = (n1<0) ? normals.length-n1 : n1-1;
+									n2 = parseFloat(n2); n2 = (n2<0) ? normals.length-n2 : n2-1;
+									n3 = parseFloat(n3); n3 = (n3<0) ? normals.length-n3 : n3-1;
+
+									part_v_indices.push(vertices[v1]);
+									part_v_indices.push(vertices[v2]);
+									part_v_indices.push(vertices[v3]);
+
+									part_n_indices.push(normals[n1]);
+									part_n_indices.push(normals[n2]);
+									part_n_indices.push(normals[n3]);
+
+									if(t1!="") {
+										t1 = parseFloat(t1); t1 = (t1<0) ? uvs.length-t1 : t1-1;
+										t2 = parseFloat(t2); t2 = (t2<0) ? uvs.length-t2 : t2-1;
+										t3 = parseFloat(t3); t3 = (t3<0) ? uvs.length-t3 : t3-1;
+
+										part_t_indices.push(uvs[t1]);
+										part_t_indices.push(uvs[t2]);
+										part_t_indices.push(uvs[t3]);
+									}
+
+									return "";
+								});
+								break;
+							default:
+								alert("CHAOS: Error while parsing .OBJ file: "+file);
+								break;
+						}
+					});
+				});
+
+ 			// CHAOS.log("vertices", vertices);
+ 			// CHAOS.log("normals", normals);
+ 			// CHAOS.log("uvs", uvs);
+
+ 			// CHAOS.log(part_v_indices);
+ 			// CHAOS.log(part_n_indices);
+ 			// CHAOS.log(part_t_indices);
+
+ 			// CHAOS.log(obj_meshes[current_mesh], obj_materials[current_material]);
+
+ 			obj_meshes[current_mesh].geometry.vertices = part_v_indices;
+ 			obj_meshes[current_mesh].geometry.normals = part_n_indices;
+ 			obj_meshes[current_mesh].geometry.uvs = part_t_indices;
+ 			obj_meshes[current_mesh].material = null;
+
+			}		// end of extract object
+
+			// two slashes -> v/v/v
+			// three slashes -> v/t v/t v/t
+			// six slashes -> v/[vt]/vn v/[vt]/vn v/[vt]/vn
+
+
+			*
+			* 1. remove comments
+			* 2. match and remove all vertices
+			* 3. match and remove all texture coords
+			* 4. match and remove all vertex normals
+			* 5. match objects ->
+			*	 create geometry
+			* 	 6. match groups and material ->
+			* 		7. match faces
+			*
+
+			// use mtl??? ___________________________________________
+
+			
+			// removing comments
+			otext = otext.replace(reg_comm, "");
+			// extracting vertices
+			otext = otext.replace(reg_v, extractVertex);
+			// extracting uvs
+			otext = otext.replace(reg_vt, extractUV);
+			// extracting normals
+			otext = otext.replace(reg_vn, extractNormal);
+
+			//extracting objects
+			var ob_num = otext.match(/\bo\b/g);
+			otext = otext.replace(/(\bo\b)/g, "#___o");
+			
+			if(ob_num==null) {
+				extractObject(null, "myOBJGeometry", otext);
+			}
+			else {
+				otext = otext.replace(reg_o, extractObject);
+			}
+
+			return obj_meshes;
+		}
+
+		return (function read_file(file, file_mat) {
+			var xmlhttp;
+
+			if (window.XMLHttpRequest) 	{ xmlhttp=new XMLHttpRequest(); }
+			else 						{ xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); }
+
+			xmlhttp.open("GET", file, false);
+			xmlhttp.send();
+			var str_obj = xmlhttp.responseText;
+
+			xmlhttp.open("GET", file_mat, false);
+			xmlhttp.send();
+			var str_mat = xmlhttp.responseText;
+
+			return parse_mat(str_mat, parse_obj, str_obj);		// async
+		})(file, matlib);
+
+};*/
+
+CHAOS.loadOBJ = function(params) {
+	var file_path = params.file ? params.file : params.path;
+	var mat_path = params.mtl ? params.mtl : file_path.replace(/\.obj/,".mtl");
+
+	var meshlib = [],
+		matlib = [],
+		current_mesh_name = "",
+		current_material_index = -1;
+
+	var all_vertices = [],
+		all_uvs = [],
+		all_normals = [];
+
+	function parse_mat(mat_str, callback, arg) {
+		mat_str.replace(/newmtl\s+(\w+)/g, function(match, mat_name) {
+			matlib[mat_name] = {};
+			return match;
+		});
+
+		// for(var key in obj_materials) {
+		// 	CHAOS.log(key, obj_materials[key]);
+		// }
+
+		// callback(arg);
+	}
+
+	function parse_obj(f_text) {
+
+		var reg_line = /([^\n]+)/g;
+
+		var reg_v  = /v[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g,
+			reg_vt = /vt[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g,
+			reg_vn = /vn[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[ \t]+(\-?[\d\.]+)[^\n]*\n/g;
+
+		var reg_f = /f\s+(.+)/g,
+			reg_slash = /\//g,
+			reg_f_vvv = /(\-?\d+)\/(\-?\d+)\/(\-?\d+)/,
+			reg_f_vt  = /(\-?\d+)\/(\-?\d+)\s+(\-?\d+)\/(\-?\d+)\s+(\-?\d+)\/(\-?\d+)/,
+			reg_f_vtn = /(\-?\d+)\/(.*)\/(\-?\d+)\s+(\-?\d+)\/(.*)\/(\-?\d+)\s+(\-?\d+)\/(.*)\/(\-?\d+)/;
+
+		f_text = f_text.replace(/(#[^\n]*\n)/g, "");
+		f_text = f_text.replace(/(\s){2,}/g, "$1");
+
+		// vertices
+		f_text = f_text.replace(reg_v, function(match, x, y, z) { all_vertices.push(new CHAOS.Vertex(parseFloat(x), parseFloat(y), parseFloat(z))); return ""; });
+		// texture coordinates
+		f_text = f_text.replace(reg_vt, function(match, u, v) { all_uvs.push( new CHAOS.UV(parseFloat(u), parseFloat(v) ) ); return ""; });
+		// normals
+		f_text = f_text.replace(reg_vn, function(match, x, y, z) { all_normals.push(new CHAOS.Vec3(parseFloat(x), parseFloat(y), parseFloat(z))); return ""; });
+
+		f_text = f_text.replace(/\bg\b([^\n]+)/g, "");
+		f_text = f_text.replace(/(\s){2,}/g, "$1");
+		
+		// read line-by-line
+		f_text.replace(reg_line, function(match, text_line) { 
+			/* o, usemtl, f
+			*
+			*	o - creates new mesh
+			*	usemtl - switches material index used
+			*	f - adds mesh face
+			*
+			*/
+			var data = [];
+			switch(text_line[0]) {
+				case "o":
+					var mesh_name = text_line.match(/o\s([^\n]+)/)[1];
+					if(meshlib[mesh_name]===undefined) {
+						
+						meshlib[mesh_name] = new CHAOS.Mesh(new CHAOS.Geometry(), matlib, []);
+						
+					}
+					current_mesh_name = mesh_name;
+					break;
+
+				case "u":
+					var material_name = text_line.match(/usemtl\s([^\n]+)/)[1];
+					var count = 0;
+					for(var key in matlib) {
+						if(key == material_name) { current_material_index = count; break; }
+						count++;
+					}
+					break;
+
+				case "f":
+					if(current_mesh_name=="") {
+						meshlib["object_1"] = new CHAOS.Mesh(new CHAOS.Geometry(), matlib, []);
+						current_mesh_name = "object_1";
+					}
+
+					// prebroj slasheve, dodaj podatke, dodaj materijal
+					switch(text_line.match(reg_slash).length) {
+						case 2:
+							break;
+						case 3:
+							break;
+						case 6:
+							data = text_line.match(reg_f_vtn);
+							for(var i=1; i<10; i++) {
+								if(data[i]!="") data[i] = parseInt(data[i]);
+							}
+
+							data[1] = (data[1]<0) ? all_vertices.length-data[1] : data[1]-1;
+							data[4] = (data[4]<0) ? all_vertices.length-data[4] : data[4]-1;
+							data[7] = (data[7]<0) ? all_vertices.length-data[7] : data[7]-1;
+
+
+							// if(data[2]!="") {
+								data[2] = (data[2]<0) ? all_uvs.length-data[2] : data[2]-1;
+								data[5] = (data[5]<0) ? all_uvs.length-data[5] : data[5]-1;
+								data[8] = (data[8]<0) ? all_uvs.length-data[8] : data[8]-1;	
+							// }
+							
+							data[3] = (data[3]<0) ? all_normals.length-data[3] : data[3]-1;
+							data[6] = (data[6]<0) ? all_normals.length-data[6] : data[6]-1;
+							data[9] = (data[9]<0) ? all_normals.length-data[9] : data[9]-1;						
+
+							meshlib[current_mesh_name].geometry.vertices.push(all_vertices[data[1]]);
+							meshlib[current_mesh_name].geometry.vertices.push(all_vertices[data[4]]);
+							meshlib[current_mesh_name].geometry.vertices.push(all_vertices[data[7]]);
+
+							// if(data[2]!="") {
+								meshlib[current_mesh_name].geometry.uvs.push(all_uvs[data[2]]);
+								meshlib[current_mesh_name].geometry.uvs.push(all_uvs[data[5]]);
+								meshlib[current_mesh_name].geometry.uvs.push(all_uvs[data[8]]);
+							
+
+							meshlib[current_mesh_name].geometry.normals.push(all_normals[data[3]]);
+							meshlib[current_mesh_name].geometry.normals.push(all_normals[data[6]]);
+							meshlib[current_mesh_name].geometry.normals.push(all_normals[data[9]]);
+
+							break;
+						default:
+							CHAOS.log("Error parsing OBJ file: "+file_path);
+							break;
+					}
+					meshlib[current_mesh_name].matFaces.push(current_material_index);
+					var l = meshlib[current_mesh_name].geometry.vertices.length/3;
+					meshlib[current_mesh_name].geometry.faces.push([3*(l-1), 3*l-2, 3*l-1]);
+					break;
+				default:
+					CHAOS.log("Error parsing OBJ file: "+file_path);
+					break;
+			}
+		}); // end reading line-by-line
+
+	}
+
+	return (function read_file(file, file_mat) {
+			var xmlhttp;
+
+			if (window.XMLHttpRequest) 	{ xmlhttp=new XMLHttpRequest(); }
+			else 						{ xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); }
+
+			xmlhttp.open("GET", file, false);
+			xmlhttp.send();
+			var str_obj = xmlhttp.responseText;
+
+			xmlhttp.open("GET", file_mat, false);
+			xmlhttp.send();
+			var str_mat = xmlhttp.responseText;
+
+			parse_mat(str_mat);
+			parse_obj(str_obj);
+
+			return meshlib;
+	})(file_path, mat_path);
+
+}
