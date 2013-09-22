e0f7023493723cb582be39edff0eeb92a12dd693
Okanovic Dragan <dragan.okan@gmail.com>
2013-09-22 17:05:33 +0200

    more geometry primitives

--- src/geometry.js
+++ src/geometry.js
@@ -14,7 +14,7 @@ var CHAOS = CHAOS || {};
 // CHAOS.Face4 ??
 
 CHAOS.Geometry = function() {
-  if(!(this instanceof arguments.callee)) {
+	if(!(this instanceof arguments.callee)) {
 		return new arguments.callee();
 	}
 	this.name = "geometry_"+arguments.callee._count++;
@@ -25,20 +25,13 @@ CHAOS.Geometry = function() {
 	this.colors = [];
 	this.uvs = [];
 
-	var buffers = [];	// private or public
 	this.needsUpdate = true;
-
-	// this.createBuffers
 
 	return this;
 };
 CHAOS.Geometry._count = 0;
 
 CHAOS.Geometry.prototype = {
-	// transform
-	// tessellate
-	// seperate faces
-	// Cylinder
 
 	reset: function() {
 		this.vertices = [];
@@ -47,8 +40,8 @@ CHAOS.Geometry.prototype = {
 		this.colors = [];
 		this.uvs = [];
 
-		var buffers = [];
 		this.needsUpdate = true;
+
 		return this;
 	},
 
@@ -58,7 +51,7 @@ CHAOS.Geometry.prototype = {
 
 		var f = this.faces;
 
-		for(var i=this.faces.length-1; i>=0; i--) {
+		for(var i=0; i<f.length; i++) {
 			// hesiranje
 			var a = this.vertices[ f[i][0] ];
 			var b = this.vertices[ f[i][1] ];
@@ -67,13 +60,12 @@ CHAOS.Geometry.prototype = {
 			var bc = CHAOS.Math.sub(c,b);
 			var ab = CHAOS.Math.sub(a,b);
 
-			var n = CHAOS.Math.cross(bc,ab);
+			var n = (CHAOS.Math.cross(bc,ab)).normalize();
 
 			this.normals.push(n);
 			this.normals.push(n);
 			this.normals.push(n);
 		}
-		return this;
 	},
 
 	Sphere: function(params) {
@@ -106,12 +98,13 @@ CHAOS.Geometry.prototype = {
                 var x = cosPhi * sinTheta;
                 var y = cosTheta;
                 var z = sinPhi * sinTheta;
-                var u = 1 - (longNumber / longitudeBands);
-                var v = 1 - (latNumber / latitudeBands);
+                var u = 1.0 - (longNumber / longitudeBands);
+                var v = 1.0 - (latNumber / latitudeBands);
 
-                this.normals.push(new CHAOS.Vec3(x,y,z));
+                this.normals.push((new CHAOS.Vec3(x,y,z)).normalize());
                 this.uvs.push( new CHAOS.UV(u,v));
                 this.vertices.push(new CHAOS.Vertex(radius*x, radius*y, radius*z));
+                this.colors.push(new CHAOS.Color().random());
             }
         }
 
@@ -121,8 +114,8 @@ CHAOS.Geometry.prototype = {
                 var first = (latNumber * (longitudeBands + 1)) + longNumber;
                 var second = first + longitudeBands + 1;
 
-                this.faces.push([first,second,first+1]);
-                this.faces.push([second, second+1, first+1]);
+                this.faces.push([first,first+1,second]);
+                this.faces.push([second+1, second, first+1]);
             }
         }
         return this;
@@ -303,7 +296,7 @@ CHAOS.Geometry.prototype = {
 			for (var j = 0; j < sh+1; j++) {
 
 				// world coord
-				this.vertices.push(new CHAOS.Vertex(i*w/sw-w/2, j*h/sh-h/2, 0));
+				this.vertices.push(new CHAOS.Vertex(i*w/sw-w/2, j*h/sh-h/2, 0.0));
 
 				// normals
 				this.normals.push(new CHAOS.Vec3(0.0, 0.0, 1.0));
@@ -316,22 +309,178 @@ CHAOS.Geometry.prototype = {
 			}
 		}
 
-
-		for(var i=0; i<sw; i++) {
-			for(var j=0; j<sh; j++) {
-				this.faces.push([(j+1)*(sw+1)+i, j*(sw+1)+i+1, j*(sw+1)+i]);
-				this.faces.push([j*(sw+1)+i+1, (j+1)*(sw+1)+i, (j+1)*(sw+1)+i+1]);
+		// indices/faces
+		for(var i=0; i<sh; i++) {
+			for(var j=0; j<sw; j++) {
+				this.faces.push([(j+1)*(sh+1)+i, j*(sh+1)+i+1, j*(sh+1)+i]);
+				this.faces.push([j*(sh+1)+i+1, (j+1)*(sh+1)+i, (j+1)*(sh+1)+i+1]);
 			}
 		}
 		return this;
 	}, 
 
-	Cylinder: function(top_radius, bottom_radius, height, segments) {
-		// ...TODO
+	Cylinder: function(params) {
+		this.name+="_cylinder";
+
+		var height = params.height ? params.height : 100;
+		var segments = params.segments ? params.segments : 6;
+		var top = params.top ? params.top : 10;
+		var bottom = params.bottom ? params.bottom : 10;
+		var caps = params.caps ? params.caps : false;
+
+		var tPI = 2*Math.PI;
+
+		this.reset();
+
+		
+		for(var i=0; i<segments; i++) {
+			var x = Math.sin(tPI/segments*i);
+			var z = Math.cos(tPI/segments*i);
+			var x1 = Math.sin(tPI/segments*(i+1));
+			var z1 = Math.cos(tPI/segments*(i+1));
+			// vertices
+			this.vertices.push(new CHAOS.Vertex(top*x,  height/2, top*z));
+			this.vertices.push(new CHAOS.Vertex(top*x1,  height/2, top*z1));
+			this.vertices.push(new CHAOS.Vertex(bottom*x, -height/2, bottom*z));
+			this.vertices.push(new CHAOS.Vertex(bottom*x1, -height/2, bottom*z1));
+
+			//colors
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());			
+
+			// uvs
+			this.uvs.push(new CHAOS.UV(i/segments,1.0));
+			this.uvs.push(new CHAOS.UV((i+1)/segments,1.0));
+			this.uvs.push(new CHAOS.UV(i/segments,0.0));
+			this.uvs.push(new CHAOS.UV((i+1)/segments,0.0));
+
+			//normals
+			var a = new CHAOS.Vec3(top*x,  height/2, top*z);
+			var b = new CHAOS.Vec3(top*x1,  height/2, top*z1);
+			var c = new CHAOS.Vec3(bottom*x, -height/2, bottom*z);
+
+			a = a.sub(b);
+			c = c.sub(b);
+
+			var n = (CHAOS.Math.cross(a,c)).normalize();
+
+			this.normals.push(n);
+			this.normals.push(n);
+			this.normals.push(n);
+			this.normals.push(n);
+
+			if(caps) {
+				// vertices
+				this.vertices.push(new CHAOS.Vertex(bottom*x, -height/2, bottom*z));
+				this.vertices.push(new CHAOS.Vertex(bottom*x1, -height/2, bottom*z1));
+				this.vertices.push(new CHAOS.Vertex(0, -height/2, 0));
+
+				this.vertices.push(new CHAOS.Vertex(top*x,  height/2, top*z));
+				this.vertices.push(new CHAOS.Vertex(top*x1,  height/2, top*z1));
+				this.vertices.push(new CHAOS.Vertex(0, height/2, 0));
+
+				// colors & uvs & normals
+				for(var k=0; k<6; k++) {
+					var y = (k<3) ? -1 : 1;
+					this.colors.push(new CHAOS.Color().random());
+					this.uvs.push(new CHAOS.UV(0.0, 0.0));
+					this.normals.push(new CHAOS.Vec3(0, y, 0));
+				}
+				
+
+				this.faces.push([10*i,   10*i+2, 10*i+1]);
+				this.faces.push([10*i+1, 10*i+2, 10*i+3]);
+				this.faces.push([10*i+4, 10*i+6, 10*i+5]);
+				this.faces.push([10*i+9, 10*i+7, 10*i+8]);
+			}
+
+			else {
+				this.faces.push([4*i,   4*i+2, 4*i+1]);
+				this.faces.push([4*i+1, 4*i+2, 4*i+3]);
+			}
+
+			
+		}
+
+		return this;
+	},
+
+	Pyramid: function(params) {
+		this.name+="_pyramid";
+
+		var height = params.height ? params.height : 100;
+		var segments = params.segments ? params.segments : 6;
+		var radius = params.radius ? params.radius : 10;
+
+		var s = Math.sqrt(height*height + radius*radius);
+		var ratio = s/(s+radius);
+
+		var tPI = 2*Math.PI;
+
+		this.reset();
+
+		for(var i=0; i<segments; i++) {
+			var x = Math.sin(tPI/segments*i);
+			var z = Math.cos(tPI/segments*i);
+			var x1 = Math.sin(tPI/segments*(i+1));
+			var z1 = Math.cos(tPI/segments*(i+1));
+			// vertices
+			
+			this.vertices.push(new CHAOS.Vertex(radius*x, -height/2, radius*z));
+			this.vertices.push(new CHAOS.Vertex(radius*x1, -height/2, radius*z1));
+			this.vertices.push(new CHAOS.Vertex(0,  height/2, 0));
+
+			this.vertices.push(new CHAOS.Vertex(radius*x, -height/2, radius*z));
+			this.vertices.push(new CHAOS.Vertex(radius*x1, -height/2, radius*z1));
+			this.vertices.push(new CHAOS.Vertex(0, -height/2, 0));
+
+			//colors
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+			this.colors.push(new CHAOS.Color().random());
+
+			//indices
+			this.faces.push([6*i, 6*i+1, 6*i+2]);
+			this.faces.push([6*i+4, 6*i+3, 6*i+5]);
+
+			// uvs
+			this.uvs.push(new CHAOS.UV(i/segments, ratio));
+			this.uvs.push(new CHAOS.UV((i+1)/segments, ratio));
+			this.uvs.push(new CHAOS.UV((2*i+1)/(2*segments), 0.0));
+
+			this.uvs.push(new CHAOS.UV(i/segments, ratio));
+			this.uvs.push(new CHAOS.UV((i+1)/segments, ratio));
+			this.uvs.push(new CHAOS.UV((2*i+1)/(2*segments), 1.0));
+
+			//normals
+			var a = new CHAOS.Vec3(radius*x,  -height/2, radius*z);
+			var b = new CHAOS.Vec3(radius*x1,  -height/2, radius*z1);
+			var c = new CHAOS.Vec3(0, height/2, 0);
+
+			a = a.sub(c);
+			b = b.sub(c);
+
+			var n = (CHAOS.Math.cross(a,b)).normalize();
+
+			this.normals.push(n);
+			this.normals.push(n);
+			this.normals.push(n);
+
+			this.normals.push(new CHAOS.Vec3(0, -1, 0));
+			this.normals.push(new CHAOS.Vec3(0, -1, 0));
+			this.normals.push(new CHAOS.Vec3(0, -1, 0));
+		}
+
+		return this;
 	},
 
 	getInfo: function() {
-		return s = this.name + ":<br>"
+		return this.name + ":<br>"
 		 	+ "-- vertices: " + this.vertices.length + "<br>"
 			+ "-- triangles: " + this.faces.length + "<br>"
 			+ "-- uvs: " + this.uvs.length + "<br>"
@@ -339,10 +488,17 @@ CHAOS.Geometry.prototype = {
 			+ "-- colors: " + this.colors.length + "<br>";
 	},
 
+	getShortInfo: function() {
+		return this.name + ": <br>v:&nbsp;{"+this.vertices.length+"}, f:&nbsp;{"+this.faces.length+"}, uv:&nbsp;{"+this.uvs.length+"}, n:&nbsp;{"+this.normals.length+"}, c:&nbsp;{"+this.colors.length+"}";
+	},
+
+	getTinyInfo: function() {
+		return this.name + ": <br>v:&nbsp;{"+this.vertices.length+"}, f:&nbsp;{"+this.faces.length+"}";
+	},
+
 	toString: function() {
 		return this.name + "[CHAOS.Geometry]";
 	}
-
 };
 
 CHAOS.Vertex = CHAOS.Vec3;
